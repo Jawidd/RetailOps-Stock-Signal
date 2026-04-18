86175adedd859e37c4b5e4df3b2716b2e92c1301
Jawidd <jawid00786@gmail.com>
2026-04-18 17:19:47 +0100

    - State Machine is Disabled - Refactor code structure for improved readability and maintainability ()

--- infrastructure/deploy-all-cfn-stacks.sh
+++ infrastructure/deploy-all-cfn-stacks.sh
@@ -16,15 +16,15 @@ echo "Region: $REGION"
 # The order is important as iamstacj import values from both stacks(s3datalake,athena)
 # lambda stack depends on ecr stack
 STACK_FILES=(
-   "retops-s3datalake.yaml" 
-   "retops-athena.yaml"
-   "retops-iam.yaml"
-   "retops-ecr-data-generator.yaml"
-   "retops-ecr-ml.yaml"                   # week5: ECR + IAM for ML
-   "retops-lambda-data-generator.yaml"
-   "retops-ecs-dbt.yaml"
-   "retops-step-functions.yaml"
-   "retops-cloudwatch.yaml"
+   "retops-s3datalake.yaml"                # Week 2: AWS Data Lake  (use 02_upload_raw_data_to_s3.py to upload raw data to s3)
+   "retops-athena.yaml"                    # week 2: Athena, Glue Catalog +week 3: add stg and mart models to glue catalog
+   "retops-iam.yaml"                       # week 2: iam roles for s3 and athena
+   "retops-ecr-data-generator.yaml"        # week 4: ECR for data generator lambda
+   "retops-ecr-ml.yaml"                    # week 5: ECR + IAM for ML
+   "retops-lambda-data-generator.yaml"     # week 4: ECR for data generator lambda
+   "retops-ecs-dbt.yaml"                   # week 5: ECS for dbt
+   "retops-step-functions.yaml"            # week 5: Step functions to orchestrate dbt and data generator lambda
+   "retops-cloudwatch.yaml"                # week 4 :CloudWatch log group: `/ecs/retailops/dev/dbt` (14 day retention)
 
 )
 
